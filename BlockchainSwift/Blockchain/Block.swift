1db6d5fdc9fc360a9d1913882ec094c828b028e1
shu223 <shuichi0526@gmail.com>
2018-01-09 18:39:28 +0900

    minor fix

--- BlockchainSwift/Blockchain/Block.swift
+++ BlockchainSwift/Blockchain/Block.swift
@@ -23,7 +23,7 @@ struct Block: Codable {
     func hash() -> Data {
         let encoder = JSONEncoder()
         let data = try! encoder.encode(self)
-        return data.sha256()!
+        return data.sha256()
     }
     
     func description() -> String {
@@ -34,8 +34,8 @@ struct Block: Codable {
 
 extension Data {
     // https://stackoverflow.com/questions/25388747/sha256-in-swift
-    func sha256() -> Data? {
-        guard let res = NSMutableData(length: Int(CC_SHA256_DIGEST_LENGTH)) else { return nil }
+    func sha256() -> Data {
+        guard let res = NSMutableData(length: Int(CC_SHA256_DIGEST_LENGTH)) else { fatalError() }
         CC_SHA256((self as NSData).bytes, CC_LONG(self.count), res.mutableBytes.assumingMemoryBound(to: UInt8.self))
         return res as Data
     }
